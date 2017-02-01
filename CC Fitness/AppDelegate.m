4b787592b5da91a3beb2572dc48eb7291a636a07
Hongxuan <hx@hongxuans-imac.local>
2017-02-01 19:51:21 +0800

    Added comment

--- CC Fitness/AppDelegate.m
+++ CC Fitness/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //This is the development branch.
     return YES;
 }
 
