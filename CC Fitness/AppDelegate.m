d3dcf172afa0f3ef8015ae0e0eebf27d2b973efc
Mark Neo <markneo@marks-macbook-air.local>
2017-02-02 13:02:28 +0800

    add comment

--- CC Fitness/AppDelegate.m
+++ CC Fitness/AppDelegate.m
@@ -14,6 +14,7 @@
 
 @implementation AppDelegate
 
+//This is a development Branch!
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
