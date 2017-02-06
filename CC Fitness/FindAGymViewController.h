0ffdb0ab852ff6c02d61daf6f9b74e1886344b19
Student2 <Student2@ccj712-07-m.local>
2017-02-06 13:53:08 +0800

    Added navigation for Find A Gym

---
+++ CC Fitness/FindAGymViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindAGymViewController.h
+//  CC Fitness
+//
+//  Created by Student2 on 6/2/17.
+//  Copyright © 2017 Hongxuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FindAGymViewController : ViewController
+
+@end
---
+++ CC Fitness/FindAGymViewController.m
@@ -0,0 +1,37 @@
+//
+//  FindAGymViewController.m
+//  CC Fitness
+//
+//  Created by Student2 on 6/2/17.
+//  Copyright © 2017 Hongxuan. All rights reserved.
+//
+
+#import "FindAGymViewController.h"
+
+@interface FindAGymViewController ()
+
+@end
+
+@implementation FindAGymViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
